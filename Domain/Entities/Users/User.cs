19fe5d8a7cf04ef1614f3e80d621fd3dc07a4bb2
Aliaa Osama <aliaaosama296@gmail.com>
2025-07-27 17:55:03 +0300

    Add User Class

---
+++ Domain/Entities/Users/User.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Users
+{
+    public class User : BaseEntity<int>
+    {
+        public required string Name { get; set; } 
+        public required string Email { get; set; }
+        public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+    }
+}
