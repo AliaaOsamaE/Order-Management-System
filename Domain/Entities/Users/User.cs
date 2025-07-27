f56d81ffb3bb2a90eb137cd215cdabbf768a3e10
Aliaa Osama <aliaaosama296@gmail.com>
2025-07-27 18:33:10 +0300

    Add User Class

---
+++ Domain/Entities/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities.Users
+{
+    public enum Role
+    {
+        Admin,
+        Customer
+    }
+
+    public class User : BaseEntity<int>
+    {
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public Role Role { get; set; }
+    }
+}
+
+    
