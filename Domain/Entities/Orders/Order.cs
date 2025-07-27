1d5b47cd5a87fe0006afb8cd9a0caeba8e32f75c
Aliaa Osama <aliaaosama296@gmail.com>
2025-07-27 18:22:40 +0300

    Add Order and Order Item Class

---
+++ Domain/Entities/Orders/Order.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Common;
+using Domain.Entities.Orders;
+using System.Net;
+
+namespace Domain.Entities.Orders
+{
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Received = 2,
+        Failed = 3,
+    }
+    public enum PaymentMethod
+    {
+        CreditCard = 1,
+        Cash = 2
+    }
+    public class Order: BaseEntity<int>
+    {
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public decimal TotalAmount { get; set; }
+        public virtual ICollection<OrderItem> orderItems { get; set; } = new HashSet<OrderItem>();
+        public OrderStatus status { get; set; } = OrderStatus.Pending;
+        public PaymentMethod paymentMethod { get; set; } = PaymentMethod.CreditCard;
+    }
+}
+
+
---
+++ Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+namespace Domain.Entities.Orders
+{
+    public class OrderItem : BaseEntity<int>
+    {
+        public int OrderId { get; set; } // FK to Order Entity
+        public virtual Order? Order { get; set; }
+
+        public int ProductId { get; set; } // FK to Product Entity
+        public virtual Product? Product { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; } = 0.0M;
+    }
+}
